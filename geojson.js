02d296dc05d1f4f3c139c1ac79608ecb6f49cda7
djprano <djprano@gmail.com>
2015-04-09 14:27:21 +0200

    subiendo

---
+++ geojson.js
@@ -0,0 +1,8 @@
+var map = L.map('map').setView([40.283677, -3.821508], 16);
+
+jQuery(document).ready(function(){
+	$.getJSON( "buildings-urjc.json", function( data ){
+		var geojsonFeature = data;	
+	})
+	L.geoJson(geojsonFeature).addTo(map);
+});
